6b4ffe200e1086477293d406326b8d9818c4d3af
DavidCB <david.corry_bass@yahoo.co.uk>
2025-12-02 11:42:43 +0000

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+const barbara = document.getElementById("barbara");
+const frosty = document.getElementById("frosty");
+const gumdrom = document.getElementById("gumdrop");
+const headElf = document.getElementById("head-elf");
+const mrFircone = document.getElementById("mr-fircone");
+const mrsClaus = document.getElementById("mrs-claus");
+const patch = document.getElementById("patch");
+const policeman = document.getElementById("policeman");
+const rudolph= document.getElementById("rudolph");
+const santa = document.getElementById("santa");
+const stableBoy = document.getElementById("stable-boy");
+const tinyTim = document.getElementById("tiny-tim");
+
+let character = ["barbara", "frosty", "gumdrop", "head-elf", "mr-fircone", "mrs-claus", "patch", "policeman", "rudolph", "santa", "scrooge", "stable-boy", "tiny-tim"];
+
+let submitBtn = document.getElementById("submitBtn");
+let suspectInput = document.getElementById("suspect");
+
+let guiltySuspect = "scrooge";  // the one who is guilty
+
+submitBtn.addEventListener('click', () => {
+    const selected = suspectInput.value.trim().toLowerCase();
+
+    if (selected === guiltySuspect) {
+        alert("it was me");
+        window.location.href = "answer.html"; /*added with help of chatgpt as resolution to revealing the whole guilty page*/
+    } else {
+        alert("it wasn't me");
+    }
+});
+
+
+
+
